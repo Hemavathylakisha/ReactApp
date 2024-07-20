1368f9871e817d4a3257fdd87102e1a55ad9d985
Hemavathylakisha <151120149+Hemavathylakisha@users.noreply.github.com>
2024-07-20 22:19:56 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Changeoptions from './components/changeoptions';
 import Todo from './components/Todo';
+import Debounce from './components/Debounce';
 
 function App() {
   return ( 
     <div className="App">
       {/* <Changeoptions /> */}
-      <Todo />
+      {/* <Todo /> */}
+      <Debounce />
     </div>
   );
 }
